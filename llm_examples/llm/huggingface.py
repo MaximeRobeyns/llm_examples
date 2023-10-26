2559e7d107c4c2cd54c6046f00f11ad276eee3d6
Maxime Robeyns <dev@maximerobeyns.com>
2023-10-26 15:32:51 +0100

    Improvements to HF LLM wrapper

--- llm_examples/llm/huggingface.py
+++ llm_examples/llm/huggingface.py
@@ -20,6 +20,7 @@ import logging
 import transformers
 
 from peft import LoraConfig, get_peft_model
+from copy import copy
 from typing import Optional
 from omegaconf import OmegaConf
 from transformers import GenerationConfig, BitsAndBytesConfig
@@ -35,29 +36,34 @@ from llm_examples.utils import (
 class HuggingFaceLLM(LLM):
     def __init__(
         self,
-        name: str,
         model_name_or_path: str,
+        name: Optional[str] = None,
         config_class: str = "AutoConfig",
         config_kwargs: dict = dict(),
-        tokenizer_class: str = "Autotokenizer",
+        tokenizer_class: str = "AutoTokenizer",
         tokenizer_kwargs: dict = dict(),
-        model_class: str = "AutoModel",
+        tokenizer_special_tokens: dict = dict(),
+        model_class: str = "AutoModelForCausalLM",
         model_kwargs: dict = dict(),
         global_gen_kwargs: dict = dict(),
         use_peft: bool = False,
         peft: Optional[LoraConfig] = None,
         quantization: Optional[BitsAndBytesConfig] = None,
     ):
+        if name is None:
+            name = model_name_or_path
         super().__init__(name)
 
         # Load the HF model config
         config_cls = getattr(transformers, config_class)
-        config_kwargs = OmegaConf.to_object(config_kwargs)
+        if not isinstance(config_kwargs, dict):
+            config_kwargs = OmegaConf.to_object(config_kwargs)
         model_config = config_cls.from_pretrained(model_name_or_path, **config_kwargs)
 
         # Load the HF model
         model_cls = getattr(transformers, model_class)
-        model_kwargs = OmegaConf.to_object(model_kwargs)
+        if not isinstance(config_kwargs, dict):
+            model_kwargs = OmegaConf.to_object(model_kwargs)
         assert isinstance(model_kwargs, dict)
         for k, v in model_kwargs.items():
             if "dtype" in k.lower() and v != "auto":
@@ -68,6 +74,8 @@ class HuggingFaceLLM(LLM):
         self.model = model_cls.from_pretrained(
             model_name_or_path, config=model_config, **model_kwargs
         )
+
+        # Configure PEFT if required
         if use_peft:
             self.peft_config = peft
             self.model = prepare_model_for_quantized_training(self.model)
@@ -75,32 +83,60 @@ class HuggingFaceLLM(LLM):
 
         # Load HF tokenizer
         tokenizer_cls = getattr(transformers, tokenizer_class)
-        tokenizer_kwargs = OmegaConf.to_object(tokenizer_kwargs)
+        if not isinstance(tokenizer_kwargs, dict):
+            tokenizer_kwargs = OmegaConf.to_object(tokenizer_kwargs)
         self.tokenizer = tokenizer_cls.from_pretrained(
             model_name_or_path,
             **tokenizer_kwargs,
         )
+        tokenizer_special_tokens = {
+            k: getattr(self.tokenizer, v.split(".")[-1])
+            for k, v in tokenizer_special_tokens.items()
+            if v.startswith("tokenizer")
+        }
+        if len(tokenizer_special_tokens) > 0:
+            self.tokenizer.add_special_tokens(tokenizer_special_tokens)
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
 
         # Load the global generation config
-        gen_cfg_kwargs = OmegaConf.to_object(global_gen_kwargs)
+        if not isinstance(global_gen_kwargs, dict):
+            global_gen_kwargs = OmegaConf.to_object(global_gen_kwargs)
         self.gen_cfg = GenerationConfig.from_pretrained(
-            model_name_or_path, **gen_cfg_kwargs
+            model_name_or_path, **global_gen_kwargs
         )
 
-    def generate(self, prompt: str, num_samples: int = 1, **kwargs) -> list[str]:
+    def generate(
+        self, prompt: str | list[str], num_samples: int = 1, **kwargs
+    ) -> list[str] | list[list[str]]:
+        """A high-level generation function for HuggingFace transformers.
+        If you require more control, use access the underlying `model` and
+        `tokenizer` fields from this model.
+        """
         device = get_fst_device(self.model)
 
-        inputs = self.tokenizer(prompt, return_tensors="pt").to(device)
+        inputs = self.tokenizer(prompt, return_tensors="pt", padding=True).to(device)
         for k in inputs.keys():
             if isinstance(inputs[k], t.Tensor):
-                inputs[k] = inputs[k].expand(num_samples, -1)
+                print(inputs[k].shape)
+                inputs[k] = inputs[k].repeat_interleave(num_samples, 0)
+                print(inputs[k].shape)
+                # inputs[k] = inputs[k].repeat_interleave(num_samples, -1)
         input_len = inputs.input_ids.size(-1)
+        gen_cfg = copy(self.gen_cfg)
+        for k, v in kwargs.items():
+            setattr(gen_cfg, k, v)
         with t.inference_mode():
-            outputs = self.model.generate(
-                **inputs, generation_config=self.gen_cfg, **kwargs
-            )
+            outputs = self.model.generate(**inputs, generation_config=gen_cfg)
         gen_tokens = outputs[:, input_len:]
-        return self.tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
+        outputs = self.tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
+        if inputs.input_ids.size(0) > 1:
+            return [
+                outputs[i : i + num_samples]
+                for i in range(0, len(outputs), num_samples)
+            ]
+        else:
+            return outputs
 
     def generate_seq_2_seq(
         self, enc_prompt: str, dec_prompt: str, **kwargs
